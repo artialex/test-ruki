98687cce419ee65a7aa2c2eed317e6b6727df065
Alexey Selivanov <lazypacman@gmail.com>
2021-02-01 16:14:27 +0300

    Fix position

--- src/modules/core/components/RootComponent.tsx
+++ src/modules/core/components/RootComponent.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { CellsPage } from '$/cells'
 
+import css from './RootComponent.module.css'
+
 export function RootComponent() {
   return (
-    <>
+    <div className={css.root}>
       <CellsPage />
-    </>
+    </div>
   )
 }
